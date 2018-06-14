b0df89969646978977d19cb2392f480d5230ef0c
gurukiratsandhu <40227390+gurukiratsandhu@users.noreply.github.com>
2018-06-13 23:45:27 -0700

    asssignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,2 +1,30 @@
+#asignment 1
+git init= git initialization
+git status= check status
+git config --global name.user"name" = adds user name
+git config--global email.user"email"= add email address
+git commit -m"message"=it for comments
+git log=to know  project activity
+$ git remote <command> <remote_name> <remote_URL>
+$ git pull <branch_name> <remote_URL/remote_name>
+
+$ git pull <branch_name> <remote_URL/remote_name>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# assignment 2
 print("hello world")
 print("my name is gurkirat")
